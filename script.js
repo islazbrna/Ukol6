fedf8a0fe90374fe8c6151f0ac8ce9e3e4112ea3
KatkaS <katerina.skocovska@gmail.com>
2024-06-04 16:47:54 +0200

    finalniverze

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const pocetLidi = 20
+const pocetLidi = 200
 
-const justFood = (pocetLidi) => {
+ const justFood = (pocetLidi) => {
     return `Catering od Just Food byl dodan pro ${pocetLidi} lidi za ${Number(pocetLidi)*500} Kč`
   }
   
@@ -11,16 +11,8 @@ const justFood = (pocetLidi) => {
   const flavourHaven = (pocetLidi) => {
     return `Catering od Flavour Haven byl dodan pro ${pocetLidi} lidi za ${Number(pocetLidi)*5000} Kč`
   }
-
 const createEvent = (nazevUdalosti, catering, pocetLidi) => {
-  if (catering == justFood) {
-  return `Událost ${nazevUdalosti} s ${justFood(pocetLidi)} `
+return `Událost ${nazevUdalosti} s ${catering(pocetLidi)}`
 }
-else if (catering == yourMama) {
-  return `Událost ${nazevUdalosti} s ${yourMama(pocetLidi)} `
-}  
-else if (catering == flavourHaven) {
- return `Událost ${nazevUdalosti} s ${flavourHaven(pocetLidi)} `
-}}
 
-console.log(createEvent('inaugurace prezidenta', flavourHaven, 10))
+console.log(createEvent('inaugurace prezidenta', justFood, 10))
